363afc07e5784989fe1142b54600c9a3a128ba64
Nicola Love <nel36@zoology.ubc.ca>
2020-11-18 12:21:44 -0800

    commit readme, vignette, and changes to Roxygen2 comments

---
+++ R/qtseries.R
@@ -0,0 +1,45 @@
+
+#' Quick Time Series Plot
+#'
+#' @param data A data frame or tibble where the inputs *time*, *voi* and *group* are stored.
+#' @param time A numeric vector that represents time (year, month, day), stored in the data set *data*.
+#' @param voi  A continuous, numeric vector stored in the data set *data* (this is your variable of interest that might have interesting variations over time).
+#' @param group A factor vector that is used to group the numeric data, *voi*, in the specified data set, *data*.
+#' @param round_values An optional argument that rounds the variable of interest (*voi*) to 3 digits (default = FALSE).
+#' @param verbose An optional argument that that keeps the user up-to-date with the ongoing processes at each step of the graphing function (default = FALSE).
+#' @param default_theme An optional argument that specifies a default theme: uses theme_minimal with bold plot title, legend title, and axis titles (default = TRUE).
+#'
+#' @importFrom ggplot2 ggplot aes geom_point geom_line labs theme_minimal theme element_text
+#'
+#' @return A ggplot object (a plot) depicting the temporal dynamics of some variable (*voi*) grouped by a factor variable (*group*) from the specified data set (*data*).
+#'
+#' @export
+#'
+#' @examples
+#' qtseries(gapminder::gapminder, year, gdpPercap, country, round_values = TRUE,verbose = TRUE)
+#'
+#' qtseries(gapminder::gapminder %>%
+#'          dplyr::filter(continent == "Americas"),
+#'          year, lifeExp, country, verbose = TRUE)
+qtseries <- function(data, time, voi, group, round_values = FALSE, verbose = FALSE, default_theme = TRUE) {
+  if(verbose)cat("Plotting time series...")
+  plot <- #if we want to round y-values
+    if(round_values){
+      ggplot(data, aes({{time}}, round({{voi}}, 3), color = {{group}}))+
+        geom_point()+
+        geom_line(alpha = .4)+
+        labs(y = deparse(substitute(voi)), title = "Quick Time Series Plot")
+    } else { # else we use raw y-values
+      ggplot(data, aes({{time}}, {{voi}}, color = {{group}}))+
+        geom_point()+
+        geom_line(alpha = .4)+
+        labs(y = deparse(substitute(voi)), title = "Quick Time Series Plot")
+    }
+  if(verbose)cat("Done labelling plot!")
+  if(default_theme){ #default theme aesthetics
+    plot <- plot + theme_minimal() + theme(plot.title = element_text(face = "bold"), axis.title = element_text(face = "bold"), legend.title = element_text(face = "bold"))
+    if(verbose)cat("...Done applying default theme aesthetics!")
+  }
+  return(plot)
+}
+
---
+++ tests/testthat/test-qtseries.R
@@ -0,0 +1,20 @@
+test_that("Quick Time Series Check", {
+  # create dummy dataframe
+  testdf <- data.frame(year = rep(seq(from = 1980, to= 1990, 1), 3),
+                       rainfall = c(rnorm(11, 100, 30), rnorm(11, 70, 10),rnorm(11, 20, 20)),
+                       temp = c(rnorm(11, 70, 30), rnorm(11, 100, 10),rnorm(11, 45, 20)),
+                       daysofsun = c(NA, NA, rnorm(9, 280, 50), rnorm(11, 300, 30), NA, rnorm(10, 340, 15)),
+                       region = c(rep("A",11), rep("B", 11), rep("C", 11)))
+
+  # test that the data is grouped as expected, testing with NA's present & all arguments = TRUE
+  expect_match("state",as.character(rlang::get_expr(qtseries(testdf, year,daysofsun,state, round_values = TRUE, verbose = TRUE)$mapping$colour)))
+  cat(" Expected grouping & extra arguments working? -> Yes!")
+
+  # test that the output is a ggplot object, testing without NA's present & all arguments = FALSE
+  expect_identical(class(qtseries(testdf, year,rainfall, state, default_theme = FALSE))[2], "ggplot")
+  cat("...Expected Object Output (ggplot)? -> Yes!")
+
+  # test that the first layer is a geom_point layer
+  expect_true("GeomPoint" %in% class(qtseries(testdf, year,temp, state)$layers[[1]]$geom))
+  cat("...Expected Geom_Point Layer? -> Yes!")
+})
